2de0adf002097055bf009cbe44b42664d9617bdb
arturobcas <80398466+arturobcas@users.noreply.github.com>
2021-03-22 19:30:43 +0100

    Add files via upload

---
+++ Menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("\t\t\t\t***********************************************\n");
+	printf("\t\t\t\t         Programa Mundial de Baloncesto\n");
+	printf("\t\t\t\t***********************************************\n\n");
+	
+	printf("\t\t\tBienvenido al programa lider en Baloncesto, encesta una con nosotros! \n\n\n");
+	
+	printf("--------ELIGE UNA OPCION PARA CONTINUAR--------------\n\n");
+	
+	printf("1--[Seleccion de Competicion]\n");
+	printf("2--[Jugadores y Estadisticas]\n");
+	printf("3--[El mejor Quinteto del mundial]\n\n");
+	printf("4--[Finalizar programa]");
+	
+	
+
+return 0;
+}
+
